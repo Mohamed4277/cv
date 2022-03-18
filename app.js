6cd252830af797bfc7d254d9e4508dfec33ed800
Mai <ma_aissaoui@yahoo.fr>
2022-03-18 19:23:19 +0100

    Redirect url

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require('express')
+const express = require('express');
+const res = require('express/lib/response');
 const app = express()
 const port = 3000
 
 app.use(express.static('public'));
+
+app.get('/',(req,res)=> res.redirect("/home.html"))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
